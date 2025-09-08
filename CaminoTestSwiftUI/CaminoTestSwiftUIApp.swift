2f0b0f5f3e2f707371511e48764a1aac825d2b03
Issam El Moujahid <issam.elmoujahid@usherbrooke.ca>
2025-09-08 01:08:02 -0400

    Initial Commit

---
+++ CaminoTestSwiftUI/CaminoTestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaminoTestSwiftUIApp.swift
+//  CaminoTestSwiftUI
+//
+//  Created by Issam EL MOUJAHID on 2025-09-08.
+//
+
+import SwiftUI
+
+@main
+struct CaminoTestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaminoTestSwiftUIUITests/CaminoTestSwiftUIUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  CaminoTestSwiftUIUITestsLaunchTests.swift
+//  CaminoTestSwiftUIUITests
+//
+//  Created by Issam EL MOUJAHID on 2025-09-08.
+//
+
+import XCTest
+
+final class CaminoTestSwiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
